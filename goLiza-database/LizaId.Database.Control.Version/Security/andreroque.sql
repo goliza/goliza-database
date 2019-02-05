bf7304a66d4ed78b5db671329a95ccdea637030f
Tiago <Santos>
2019-02-05 17:46:12 -0200

    ajuster nas pastas da solution remoção do usuário andre

--- goLiza-database/LizaId.Database.Control.Version/Security/andreroque.sql
+++
@@ -1,2 +0,0 @@
-﻿CREATE USER [andreroque] FOR LOGIN [andreroque];
-
--- goLiza-database/LizaId.Database.Control.Version/Security/andreroque_1.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [andreroque]
-    WITH PASSWORD = N'Qwzu7y3x{p{chuoMumsrneapmsFT7_&#$!~<@sLnu8{qunf^', SID = 0x90E2F2D1170A5C45940D0AD7D107F844, DEFAULT_LANGUAGE = [us_english];
-
