c7f7dbff2c3ba6c390661284562f7e1761c30331
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-02-11 18:12:16 -0200

    inclusao chave primaria table pendencia

--- source/LizaId.Database.Control.Version/dbo/Tables/Pendencia.sql
+++ source/LizaId.Database.Control.Version/dbo/Tables/Pendencia.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Pendencia] (
-    [idPendencia]              INT           NOT NULL,
+    [idPendencia]              INT           IDENTITY (1, 1) NOT NULL,
     [idUsuarioEmpresa]         INT           NULL,
     [idEmpresaDestinataria]    INT           NOT NULL,
     [EmailDestinatario]        VARCHAR (100) NOT NULL,
@@ -11,6 +11,7 @@
     [DataCriacao]              DATETIME      NOT NULL,
     [idUsuarioUltimaAlteracao] INT           NULL,
     [DataUltimaAlteracao]      DATETIME      NULL,
+	CONSTRAINT [PK_Pendencia] PRIMARY KEY CLUSTERED ([idPendencia] ASC),
     CONSTRAINT [FK_Pendencia_EmpresaReceptora] FOREIGN KEY ([idEmpresaDestinataria]) REFERENCES [dbo].[EmpresaReceptora] ([idEmpresaReceptora]),
     CONSTRAINT [FK_Pendencia_UsuarioEmpresa] FOREIGN KEY ([idUsuarioEmpresa]) REFERENCES [dbo].[UsuarioEmpresa] ([idUsuarioEmpresa]),
 	CONSTRAINT [FK_Pendencia_GrupoInformacao] FOREIGN KEY ([idGrupoInformacao]) REFERENCES [dbo].[GrupoInformacao] ([idGrupoInformacao])
