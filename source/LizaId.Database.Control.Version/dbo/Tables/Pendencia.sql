4cf80a30b37c8b838c212addc62ba541984ed285
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-02-11 16:24:04 -0200

    #501 - alteração tabela pendencia

--- source/LizaId.Database.Control.Version/dbo/Tables/Pendencia.sql
+++ source/LizaId.Database.Control.Version/dbo/Tables/Pendencia.sql
@@ -4,7 +4,7 @@
     [idEmpresaDestinataria]    INT           NOT NULL,
     [EmailDestinatario]        VARCHAR (100) NOT NULL,
     [AssuntoPendencia]         VARCHAR (200) NOT NULL,
-    [GrupoInformacao]          INT           NULL,
+    [idGrupoInformacao]          INT           NULL,
     [DescricaoPendencia]       VARCHAR (MAX) NULL,
     [StatusPendencia]          BIT           NOT NULL,
     [idUsuarioCriacao]         INT           NOT NULL,
@@ -12,6 +12,7 @@
     [idUsuarioUltimaAlteracao] INT           NULL,
     [DataUltimaAlteracao]      DATETIME      NULL,
     CONSTRAINT [FK_Pendencia_EmpresaReceptora] FOREIGN KEY ([idEmpresaDestinataria]) REFERENCES [dbo].[EmpresaReceptora] ([idEmpresaReceptora]),
-    CONSTRAINT [FK_Pendencia_UsuarioEmpresa] FOREIGN KEY ([idUsuarioEmpresa]) REFERENCES [dbo].[UsuarioEmpresa] ([idUsuarioEmpresa])
+    CONSTRAINT [FK_Pendencia_UsuarioEmpresa] FOREIGN KEY ([idUsuarioEmpresa]) REFERENCES [dbo].[UsuarioEmpresa] ([idUsuarioEmpresa]),
+	CONSTRAINT [FK_Pendencia_GrupoInformacao] FOREIGN KEY ([idGrupoInformacao]) REFERENCES [dbo].[GrupoInformacao] ([idGrupoInformacao])
 );
 
