974d2415f0e59c71c8ff73bd6c4c559e6f941941
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-02-13 17:56:19 -0200

    #511, #513, #516 - procs crud pendencia

---
+++ source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaAlterar.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[Proc_PendenciaAlterar]
+	@idPendencia int,
+    @EmailDestinatario varchar(MAX),
+    @AssuntoPendencia varchar(MAX),
+	@IdGrupoInformacao int, 
+	@DescricaoPendencia int,
+	@idUsuarioUltimaAlteracao int,
+	@DataUltimaAlteracao datetime
+AS 
+BEGIN
+	UPDATE [dbo].[Pendencia]
+	SET    [EmailDestinatario] = @EmailDestinatario, [AssuntoPendencia] = @AssuntoPendencia, [IdGrupoInformacao] = @IdGrupoInformacao, [DescricaoPendencia] = @DescricaoPendencia, [idUsuarioUltimaAlteracao] = @idUsuarioUltimaAlteracao, [DataUltimaAlteracao] = @DataUltimaAlteracao
+	WHERE  [idPendencia] = @idPendencia
+	
+	RETURN (@idPendencia)
+END
