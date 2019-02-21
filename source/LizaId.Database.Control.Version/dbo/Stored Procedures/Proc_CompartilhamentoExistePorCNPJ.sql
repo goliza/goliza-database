ce02267d3b0179fceeafa42b27a9777fba5ee941
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-02-21 10:28:44 -0300

    #583 - correção proc que checa se ja existe compartilhamento

--- source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_CompartilhamentoExistePorCNPJ.sql
+++ source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_CompartilhamentoExistePorCNPJ.sql
@@ -1,11 +1,14 @@
 ﻿create PROC [dbo].[Proc_CompartilhamentoExistePorCNPJ] 
-	@cnpj varchar(max)
+	@cnpj varchar(max),
+	@idEmpresaProvedora int
 AS 
 BEGIN
 	DECLARE @Qtd INTEGER;
 
-	SELECT @Qtd = COUNT([idCompartilhamento]) FROM [dbo].[Compartilhamento]	
-	WHERE [CNPJEmpresaReceptora] = @cnpj
+	SELECT @Qtd = COUNT([idCompartilhamento]) FROM [dbo].[Compartilhamento]	c
+	LEFT OUTER JOIN EmpresaReceptora AS R ON C.CNPJEmpresaReceptora = R.CNPJEmpresa
+	WHERE (C.CNPJEmpresaReceptora = @cnpj)
+	AND C.idEmpresaProvedora = @idEmpresaProvedora
 
 	RETURN (@Qtd)
 END
