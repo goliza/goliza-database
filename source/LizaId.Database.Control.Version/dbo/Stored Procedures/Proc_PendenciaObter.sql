0c716d5c8e2c2739ef6415d78061e2b3602a5ced
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-03-06 22:59:59 -0300

    criação [dbo].[Proc_PendenciaObter]

---
+++ source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaObter.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[Proc_PendenciaObter]
+	@idPendencia int = 0
+AS
+BEGIN
+	SELECT 
+		idPendencia
+		,idUsuarioEmpresa
+		,idEmpresaDestinataria
+		,EmailDestinatario
+		,AssuntoPendencia
+		,idGrupoInformacao
+		,DescricaoPendencia
+		,StatusPendencia
+		,idUsuarioCriacao
+		,DataCriacao
+		,idUsuarioUltimaAlteracao
+		,DataUltimaAlteracao
+	FROM Pendencia  p
+	WHERE p.idPendencia = @idPendencia
+END
+
