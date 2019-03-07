1afad5b61fac6ca3aceed04accd3f03d16daaf1f
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-03-07 08:13:25 -0300

    aujstes procs pendencia alterar e listar

--- source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaAlterar.sql
+++ source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaAlterar.sql
@@ -3,13 +3,14 @@
     @EmailDestinatario varchar(MAX),
     @AssuntoPendencia varchar(MAX),
 	@IdGrupoInformacao int, 
-	@DescricaoPendencia int,
+	@DescricaoPendencia varchar(max),
 	@idUsuarioUltimaAlteracao int,
-	@DataUltimaAlteracao datetime
+	@DataUltimaAlteracao datetime,
+	@StatusPendencia varchar(max)
 AS 
 BEGIN
 	UPDATE [dbo].[Pendencia]
-	SET    [EmailDestinatario] = @EmailDestinatario, [AssuntoPendencia] = @AssuntoPendencia, [IdGrupoInformacao] = @IdGrupoInformacao, [DescricaoPendencia] = @DescricaoPendencia, [idUsuarioUltimaAlteracao] = @idUsuarioUltimaAlteracao, [DataUltimaAlteracao] = @DataUltimaAlteracao
+	SET    [EmailDestinatario] = @EmailDestinatario, [AssuntoPendencia] = @AssuntoPendencia, [IdGrupoInformacao] = @IdGrupoInformacao, [DescricaoPendencia] = @DescricaoPendencia, [idUsuarioUltimaAlteracao] = @idUsuarioUltimaAlteracao, [DataUltimaAlteracao] = @DataUltimaAlteracao, [StatusPendencia] = @StatusPendencia
 	WHERE  [idPendencia] = @idPendencia
 	
 	RETURN (@idPendencia)
--- source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaListar.sql
+++ source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaListar.sql
@@ -1,9 +1,28 @@
 ﻿CREATE PROCEDURE [dbo].[Proc_PendenciaListar]
-	@IdEmpresa INT,
+	@IdEmpresa INT = 0,
+	@cnpj varchar(max) = '',
+	@statusPendencia bit = 1,
+	@mesAno varchar(10) = '',
+	@dataInicio datetime, 
+	@dataFim datetime,
+	@subgrupo int = 0, 
+	@idUsuarioEmpresa int = 0,
 	@Inicio INT = 0, 
 	@Total INT = 1
 AS 
 BEGIN
+	
+	if len(@dataInicio)>0
+	set @dataInicio = CONCAT(CONVERT(varchar(10), @dataInicio, 103), ' 00:00:00')
+	else
+	set @dataInicio = CONCAT(CONVERT(varchar(10), dateadd(DAY,-30, GETDATE()), 103), ' 00:00:00')
+
+
+	if len(@dataFim)>0
+	set @dataFim = CONCAT(CONVERT(varchar(10),@dataFim,103), ' 23:59:59')
+	else
+	set @dataFim = getdate()
+
 	SELECT 
 		idPendencia
 		,idUsuarioEmpresa
@@ -17,8 +36,42 @@ BEGIN
 		,DataCriacao
 		,idUsuarioUltimaAlteracao
 		,DataUltimaAlteracao
-	FROM [dbo].[Pendencia]
-	WHERE [idEmpresaDestinataria] = @IdEmpresa
+	FROM [dbo].[Pendencia] p
+	INNER JOIN [dbo].EmpresaReceptora er ON er.idEmpresaReceptora = p.idEmpresaDestinataria
+	WHERE 
+		(@IdEmpresa = 
+		case when @IdEmpresa > 0
+		then p.[idEmpresaDestinataria]
+		else @IdEmpresa
+		end)
+	AND (@cnpj = 
+		case when len(@cnpj) > 0
+		then er.CNPJEmpresa
+		else @cnpj
+		end)
+	AND (Month(@mesAno) =
+		case when len(@mesAno) > 0
+		then MONTH(p.DataCriacao)
+		else Month(@mesAno)
+		end)
+	AND (Year(@mesAno) =
+		case when len(@mesAno) > 0
+		then YEAR(p.DataCriacao)
+		else Year(@mesAno)
+		end)
+	AND p.StatusPendencia = @statusPendencia
+	AND p.DataCriacao between @dataInicio AND @dataFim
+	AND (@subgrupo =
+		case when @subgrupo > 0
+		then p.idGrupoInformacao
+		else @subgrupo
+		end)
+	AND (idUsuarioEmpresa =
+		case when idUsuarioEmpresa > 0
+		then p.idUsuarioCriacao
+		else idUsuarioEmpresa
+		end)
+
 	ORDER BY [idPendencia] DESC
 	OFFSET @Inicio ROW
 	FETCH NEXT @Total ROWS ONLY
