ed3da68ad39ac54253872c11817d77a3c544ab2e
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-03-08 11:46:49 -0300

    correções [Proc_PendenciaListar]

--- source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaListar.sql
+++ source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_PendenciaListar.sql
@@ -3,20 +3,26 @@
 	@cnpj varchar(max) = '',
 	@statusPendencia bit = 1,
 	@mesAno varchar(10) = '',
-	@dataInicio datetime, 
-	@dataFim datetime,
+	@dataInicio varchar(20), 
+	@dataFim varchar(20),
 	@subgrupo int = 0, 
 	@idUsuarioEmpresa int = 0,
 	@Inicio INT = 0, 
-	@Total INT = 1
+	@Total INT = 10
 AS 
 BEGIN
+
+	set dateformat dmy
 	
+	if(ISNULL(@mesAno, '') = '')
+	set @mesAno = ''
+	else
+	set @mesAno = CONCAT('01/', @mesAno)
+
 	if len(@dataInicio)>0
 	set @dataInicio = CONCAT(CONVERT(varchar(10), @dataInicio, 103), ' 00:00:00')
 	else
 	set @dataInicio = CONCAT(CONVERT(varchar(10), dateadd(DAY,-30, GETDATE()), 103), ' 00:00:00')
-
 
 	if len(@dataFim)>0
 	set @dataFim = CONCAT(CONVERT(varchar(10),@dataFim,103), ' 23:59:59')
@@ -49,12 +55,12 @@ BEGIN
 		then er.CNPJEmpresa
 		else @cnpj
 		end)
-	AND (Month(@mesAno) =
+	AND (Month(ISNULL(@mesAno,'')) =
 		case when len(@mesAno) > 0
 		then MONTH(p.DataCriacao)
 		else Month(@mesAno)
 		end)
-	AND (Year(@mesAno) =
+	AND (Year(ISNULL(@mesAno,'')) =
 		case when len(@mesAno) > 0
 		then YEAR(p.DataCriacao)
 		else Year(@mesAno)
@@ -66,13 +72,12 @@ BEGIN
 		then p.idGrupoInformacao
 		else @subgrupo
 		end)
-	AND (idUsuarioEmpresa =
-		case when idUsuarioEmpresa > 0
+	AND (@idUsuarioEmpresa =
+		case when @idUsuarioEmpresa > 0
 		then p.idUsuarioCriacao
-		else idUsuarioEmpresa
+		else @idUsuarioEmpresa
 		end)
-
-	ORDER BY [idPendencia] DESC
-	OFFSET @Inicio ROW
-	FETCH NEXT @Total ROWS ONLY
+		ORDER BY [idPendencia] DESC
+		OFFSET @Inicio ROW
+		FETCH NEXT @Total ROWS ONLY
 END
