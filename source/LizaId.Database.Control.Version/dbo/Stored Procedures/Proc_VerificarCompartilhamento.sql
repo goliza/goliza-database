bbb96d5cc5975fab32c225eaf148ce557c010b43
Victor Emiliano Olive Gonzalez <victor.gonzalez@dealbr.com.br>
2019-03-12 19:31:38 -0300

    #636 - ajuste verificação de acesso

--- source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_VerificarCompartilhamento.sql
+++ source/LizaId.Database.Control.Version/dbo/Stored Procedures/Proc_VerificarCompartilhamento.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROC [dbo].[Proc_VerificarCompartilhamento] --[dbo].[Proc_VerificarCompartilhamento] 21, 101, 1
     @idUsuario int,
-	@codigoGrupoInformacao int,
+	@codigoGrupoInformacao int=0,
 	@idEmpresaProvedora int
 AS 
 BEGIN
@@ -9,7 +9,12 @@ BEGIN
 	INNER JOIN EmpresaReceptora AS E ON E.idEmpresaReceptora = UE.IDEMPRESA
 	INNER JOIN Compartilhamento AS C ON C.CNPJEmpresaReceptora = E.CNPJEmpresa and c.idEmpresaProvedora = @idEmpresaProvedora
 	INNER JOIN GrupoInformacaoCompartilhamento AS G ON C.idCompartilhamento = G.idCompartilhamento
-	WHERE (G.idGrupoInformacao = @codigoGrupoInformacao)
+	WHERE
+	(@codigoGrupoInformacao = 
+		case when @codigoGrupoInformacao > 0
+		then G.idGrupoInformacao
+		else @codigoGrupoInformacao
+		end)
 	AND UE.idUsuario = @idUsuario
 END
 
