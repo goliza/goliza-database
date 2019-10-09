95f5614f1ac4df91eb5d0590059cb88041447f4f
Tiago Santos <tfsantosbr@gmail.com>
2019-10-09 13:39:59 -0300

    Backup das procedures removidas

---
+++ history/procedures/Proc_ContatoListar.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[Proc_ContatoListar] @IdEmpresa INT,
+	@Inicio INT = 0, 
+	@Total INT = 1
+AS 
+BEGIN
+	SELECT 
+		[idContato]
+		,[idEmpresa]
+		,[NomeContato]
+		,[SobrenomeContato]
+		,[CargoContato]
+		,[idTipoContato]
+	FROM [dbo].[Contato]
+	WHERE [idEmpresa] = @IdEmpresa
+	ORDER BY [DataAlteracao] DESC
+	OFFSET @Inicio ROW
+	FETCH NEXT @Total ROWS ONLY
+END
